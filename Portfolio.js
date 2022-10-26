dc41493cb911ff0c8c2922ad254c1a5fd1fead49
SarahCianciola20 <99061395+SarahCianciola20@users.noreply.github.com>
2022-10-25 23:43:30 -0500

    10/25/22

--- Portfolio.js
+++ Portfolio.js
@@ -7,7 +7,11 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  let dist = 600;
+  if (
+    document.body.scrollTop > dist ||
+    document.documentElement.scrollTop > dist
+  ) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -42,3 +46,25 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
