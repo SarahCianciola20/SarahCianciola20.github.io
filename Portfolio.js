889aed15bd7f8b4842d04991dff1479f9e4ef408
SarahCianciola20 <99061395+SarahCianciola20@users.noreply.github.com>
2022-11-08 20:07:13 -0600

    11/8/22

--- Portfolio.js
+++ Portfolio.js
@@ -139,3 +139,33 @@ function activateChapter(chapters, className) {
 //   }
 //   x[slideIndex - 1].style.display = 'block'
 // }
+let slideIndex = 1
+showSlides(slideIndex)
+
+function plusSlides(n) {
+  showSlides((slideIndex += n))
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n))
+}
+
+function showSlides(n) {
+  let i
+  let slides = document.getElementsByClassName('mySlides')
+  let dots = document.getElementsByClassName('dot')
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active2', '')
+  }
+  slides[slideIndex - 1].style.display = 'block'
+  dots[slideIndex - 1].className += ' active2'
+}
